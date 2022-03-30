0524ab61f51df9a122b60de89d04600d8b2c9b84
claudiu <danbalint_88@yahoo.com>
2022-03-30 12:07:49 +0300

    CB: Restful Web Services - added customized exception handler

--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/user/UserController.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/user/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
