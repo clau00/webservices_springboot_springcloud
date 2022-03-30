744267f8869620211ed26ce3cc7a63c74f3157a9
claudiu <danbalint_88@yahoo.com>
2022-03-30 16:57:57 +0300

    CB: Restful Web Services - added internationalization to hello_world

--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.learning.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	MessageSource messageSource;
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -20,6 +26,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
 	}
 
 }
