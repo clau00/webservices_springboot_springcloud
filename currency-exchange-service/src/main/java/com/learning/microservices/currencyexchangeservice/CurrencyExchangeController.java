061a4997fa9d694522672f6cb9eaa0b9fc8f5e12
clau00 <danbalint_88@yahoo.com>
2022-04-04 15:59:24 +0300

    CB: microservices: added repository and adjusted the controller

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -8,16 +8,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @RestController
 public class CurrencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
 
     @Autowired
     private Environment environment;
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+//        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        Optional<CurrencyExchange> currencyExchangeOpt = repository.findByFromAndTo(from, to);
+
+        if(!currencyExchangeOpt.isPresent()) {
+            throw  new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+
+        CurrencyExchange currencyExchange = currencyExchangeOpt.get();
 
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
---
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,11 @@
+package com.learning.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    Optional<CurrencyExchange> findByFromAndTo(String from, String to);
+}
